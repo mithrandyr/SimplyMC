ecb2d786e4384eea3fb947cabdfdc73d958beb3b
mithrandyr <mithrandyr@users.noreply.github.com>
2020-12-01 22:59:17 -0500

    restructuring

--- SimplyMC/SimplyMC.psm1
+++ SimplyMC/SimplyMC.psm1
@@ -1,5 +1,5 @@
-if($PSVersionTable.PSEdition -eq "Core") { Add-Type -Path "$PSScriptRoot\bin\core\CmlLib.dll" }
-else { Add-Type -Path "$PSScriptRoot\bin\winps\CmlLib.dll" }
+if($PSVersionTable.PSEdition -eq "Core") { Add-Type -Path "$PSScriptRoot\lib\netcoreapp3.1\CmlLib.dll" }
+else { Add-Type -Path "$PSScriptRoot\lib\net462\CmlLib.dll" }
 
 foreach ($f in Get-ChildItem -Path "$PSScriptRoot\functions" -Filter *.ps1) {
     . $f.FullName
